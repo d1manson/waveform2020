b50b949911bfb663b2d952c1a6b7741752b49346
Daniel Manson <danielmanson.uk@gmail.com>
2020-12-30 17:13:23 +0000

    application panes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="main">
-    <file-explorer />
+    <splitpanes vertical class="default-theme">
+      <pane size="30">
+        <splitpanes horizontal class="default-theme">
+          <pane size="30">
+            <file-explorer />
+          </pane>
+          <pane size="35">
+            TODO: plot pos data here
+          </pane>
+          <pane size="35">
+            TODO: plot clusters here
+          </pane>
+        </splitpanes></pane
+      >
+      <pane size="70">
+        TODO: Tilewall
+      </pane>
+    </splitpanes>
     <file-drop />
   </div>
 </template>
@@ -8,24 +25,31 @@
 <script>
 import FileDrop from "./components/file_drop.vue";
 import FileExplorer from "./components/file_explorer.vue";
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
 
 export default {
   name: "App",
   components: {
     FileDrop,
     FileExplorer,
+    Splitpanes,
+    Pane,
   },
 };
 </script>
 
 <style scoped>
 .main {
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-  position: fixed;
-  left: 0;
-  top: 0;
+  width: 100%;
+  height: 100%;
   font-family: Arial, Helvetica, sans-serif;
+}
+.splitpanes__pane {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2) inset;
 }
 </style>
--- src/components/file_explorer.vue
+++ src/components/file_explorer.vue
@@ -1,52 +1,67 @@
 <template>
   <div class="file-explorer">
     <template v-if="experiments.length">
-      <div
-        v-for="experiment in experiments"
-        :key="experiment.name"
-        class="experiment"
-      >
-        <div class="experiment-top">
-          <div class="experiment-name">trial '{{ experiment.name }}'</div>
-          <div
-            class="file set-file"
-            v-if="experiment.set_file"
-            :title="experiment.set_file.name"
-          >
-            {{ experiment.set_file.short }}
-          </div>
-          <div
-            class="file pos-file"
-            v-if="experiment.pos_file"
-            :title="experiment.pos_file.name"
-          >
-            {{ experiment.pos_file.short }}
-          </div>
-        </div>
-        <div class="experiment-bottom">
-          <div
-            v-for="tet in experiment.tetrodes.filter((tet) => !!tet)"
-            :key="tet.num"
-            class="tet-file-group"
-          >
+      <div class="experiment-list">
+        <div
+          v-for="experiment in experiments"
+          :key="experiment.name"
+          class="experiment"
+          @click="selectedExperimentName = experiment.name"
+          :class="{ selected: experiment.name === selectedExperimentName }"
+        >
+          <div class="experiment-top">
+            <div class="experiment-name">trial '{{ experiment.name }}'</div>
             <div
-              class="file tet-file"
-              v-if="tet.tet_file"
-              :title="tet.tet_file.name"
+              class="file set-file"
+              v-if="experiment.set_file"
+              :title="experiment.set_file.name"
             >
-              {{ tet.tet_file.short }}
+              {{ experiment.set_file.short }}
             </div>
+            <div
+              class="file pos-file"
+              v-if="experiment.pos_file"
+              :title="experiment.pos_file.name"
+            >
+              {{ experiment.pos_file.short }}
+            </div>
+          </div>
+          <div class="experiment-bottom">
+            <div
+              v-for="tet in experiment.tetrodes.filter((tet) => !!tet)"
+              :key="tet.num"
+              class="tet-file-group"
+              @click="selectedTetNum = tet.num"
+            >
+              <div
+                class="file tet-file"
+                :class="{ small: selectedTetNum !== tet.num }"
+                v-if="tet.tet_file"
+                :title="tet.tet_file.name"
+              >
+                {{ tet.tet_file.short }}
+              </div>
 
-            <div
-              class="file cut-file"
-              v-for="cut_file in tet.cut_files"
-              :key="cut_file.name"
-              :title="cut_file.name"
-            >
-              {{ cut_file.short }}
+              <div
+                class="file cut-file"
+                :class="{ small: selectedTetNum !== tet.num }"
+                v-for="cut_file in tet.cut_files"
+                :key="cut_file.name"
+                :title="cut_file.name"
+              >
+                {{ cut_file.short }}
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div class="state-info">
+        Selected trial '{{ selectedExperimentName }}',
+        {{
+          selectedTetNum
+            ? `tetrode #${selectedTetNum}`
+            : "but no tetrode selected"
+        }}.
       </div>
     </template>
     <div v-else class="empty">
@@ -69,26 +84,67 @@ export default {
   },
   data: () => ({
     experiments: [],
+    selectedExperimentName: null,
+    selectedTetNum: null,
+    selectedCutName: null,
   }),
   mounted() {
     window.fe = this;
   },
   methods: {},
+  computed: {
+    selectedExperiment() {
+      return this.experiments.find(
+        (exp) => exp.name === this.selectedExperimentName
+      );
+    },
+  },
+  watch: {
+    experiments() {
+      if (this.experiments.length && !this.selectedExperimentName) {
+        this.selectedExperimentName = this.experiments[0].name;
+      }
+    },
+    selectedExperiment() {
+      if (this.selectedExperiment && !this.selectedTetNum) {
+        const tet = this.selectedExperiment.tetrodes.find((t) => !!t);
+        this.selectedTetNum = tet && tet.num;
+      }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .file-explorer {
-  width: 400px;
+  width: 100%;
+  height: 100%;
   padding: 10px 2px;
   margin: 0 auto;
-  background: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.experiment-list {
+  overflow-y: auto;
+  flex-grow: 1;
+}
+.state-info {
+  flex-grow: 0;
+  flex-shrink: 0;
+  font-size: 0.6em;
+  padding: 5px 0px 5px 6px;
+  border-top: 1px solid #c3c0c0;
 }
 .experiment {
   margin: 4px 2px;
   border: 1px solid #d0d0d0;
-  padding: 4px 2px;
+  padding: 4px 6px;
   background: white;
+  cursor: pointer;
+  &.selected {
+    background: #f1efb6;
+  }
 }
 .file {
   font-size: 0.8em;
@@ -96,7 +152,10 @@ export default {
   margin-left: 4px;
   padding: 1px 2px;
   border: 1px solid;
-
+  &.small {
+    width: 3px;
+    overflow: hidden;
+  }
   &.pos-file,
   &.set-file {
     background: #e6d39c;
@@ -125,12 +184,14 @@ export default {
 .tet-file-group {
   display: inline-block;
   margin-right: 4px;
+  vertical-align: top;
+  line-height: 0.8em;
 }
 h4 {
   margin: 5px 0px;
 }
 .empty {
-  margin: 4px 2px;
+  margin: 4px 20px;
   border: 1px solid #d0d0d0;
   padding: 10px;
   text-align: center;
